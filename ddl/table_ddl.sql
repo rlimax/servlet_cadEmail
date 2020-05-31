aa38f9a6f0faa395e6468996326b9d4b157b0129
rlimax <rodrigoetb@gmail.com>
2020-05-31 12:54:12 -0300

    Alteração de servidor de BD e atualização de tabela.

--- ddl/table_ddl.sql
+++ ddl/table_ddl.sql
@@ -1,4 +1,6 @@
-create table usuario(
-    id int primary key AUTO_INCREMENT,
-	email varchar(100)
+create table tb_usuario(
+	id int primary key AUTO_INCREMENT,
+	nome varchar(100) not null,
+	email varchar(100) not null,
+	mensagem varchar(110)
 );
